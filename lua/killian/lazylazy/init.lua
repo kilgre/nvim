895705b1a717b90b2fbe8566265aa2aaf4ed826c
Killian <killiangreene6@gmail.com>
2024-10-10 04:14:45 -0400

    remove prints, added (not working) auto lsp install

--- lua/killian/lazylazy/init.lua
+++ lua/killian/lazylazy/init.lua
@@ -147,15 +147,30 @@ return {
           dependencies = { "mason-lspconfig" },
           config =
               function()
-                  print("loading?")
                   local on_att =
                       function(_, _)
-                          print("lspconfig setup")
                           vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
                       end
                   require("lspconfig").lua_ls.setup({
                       on_attach = on_att
                   })
+                  -- setup mason auto install
+                  -- No clue how to fix this
+--                local mason_lspconfig = require("mason-lspconfig")
+--                -- auto handlers
+--                mason_lspconfig.setup_handlers({
+--                    -- The first entry (without a key) will be the default handler
+--                    -- and will be called for each installed server that doesn't have
+--                    -- a dedicated handler.
+--                    function (server_name) -- default handler (optional)
+--                        require("lspconfig")[server_name].setup {}
+--                    end,
+--                    -- Next, you can provide a dedicated handler for specific servers.
+--                    -- For example, a handler override for the `rust_analyzer`:
+--                    ["rust_analyzer"] = function ()
+--                        require("rust-tools").setup {}
+--                    end
+--                })
               end
       },
       -- completions stuff
