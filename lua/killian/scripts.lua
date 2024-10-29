b2bf95b8800de63ab95334b555492753783be70a
Killian Greene <kilgre@amazon.com>
2024-10-29 20:38:58 +0000

    trim whitespace on yank info message

--- lua/killian/scripts.lua
+++ lua/killian/scripts.lua
@@ -3,7 +3,8 @@ local function yank_feedback()
     local yanked_text = vim.fn.getreg(register)
     local max_length = 28
 
-    -- Truncate the text if it's too long
+    -- Trim whitespace and truncate
+    yanked_text = string.gsub(yanked_text, '^%s*(.-)%s*$', '%1')
     if #yanked_text > max_length then
         local truncation_suffix = "..."
         yanked_text = yanked_text:sub(1, max_length - string.len(truncation_suffix)) .. truncation_suffix
@@ -13,7 +14,7 @@ local function yank_feedback()
     yanked_text = yanked_text:gsub("\n", "\\n"):gsub("\t", "\\t")
 
     -- Display the message in the status bar
-    local message = string.format("'%s' yanked into register '%s'", yanked_text, register)
+    local message = string.format("`%s` yanked into register `%s`", yanked_text, register)
     vim.api.nvim_echo({{message, "None"}}, false, {})
 end
 
